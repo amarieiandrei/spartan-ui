9f1c4508358707cc7e07d39e8c4c6bc4ad7c7352
Andrei Amariei <andrei.amariei@arobs.com>
2024-03-27 11:28:47 +0200

    new-control-flow

--- apps/ediscovery-angular-v16/src/app/components/new-controls-flow/new-controls-flow.component.ts
+++ apps/ediscovery-angular-v16/src/app/components/new-controls-flow/new-controls-flow.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Directives
+import { HlmSeparatorDirective } from 'libs/ui/ui-separator-helm/src';
+
 @Component({
   selector: 'spartan-new-controls-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HlmSeparatorDirective],
   templateUrl: './new-controls-flow.component.html',
 })
-export class NewControlsFlowComponent {}
+export class NewControlsFlowComponent { }
